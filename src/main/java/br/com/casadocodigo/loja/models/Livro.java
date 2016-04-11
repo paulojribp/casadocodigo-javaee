d085ec4bf68c451c37f8dd03a4f34e4d7189132a
Paulo Jr <paulojribp@gmail.com>
2016-04-11 19:19:41 -0300

    aula1-video4-criando-o-dao-e-inicio-jta

--- src/main/java/br/com/casadocodigo/loja/models/Livro.java
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -2,9 +2,20 @@ package br.com.casadocodigo.loja.models;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
 public class Livro {
 
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
 	private String titulo;
+	@Lob
 	private String descricao;
 	private BigDecimal preco;
 	private Integer numeroPaginas;
