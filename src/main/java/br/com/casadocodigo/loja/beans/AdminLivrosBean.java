eeb1ca5845a0da94b5fd440d82fcbe15a517a726
Paulo Jr <paulojribp@gmail.com>
2016-04-11 20:30:47 -0300

    aula1-video6-injection-com-cdi-e-beans-xml

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,8 +1,11 @@
 package br.com.casadocodigo.loja.beans;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.transaction.Transactional;
 
+import br.com.casadocodigo.loja.daos.LivroDao;
 import br.com.casadocodigo.loja.models.Livro;
 
 @Named
@@ -10,8 +13,13 @@ import br.com.casadocodigo.loja.models.Livro;
 public class AdminLivrosBean {
 	
 	private Livro livro = new Livro();
+	
+	@Inject
+	private LivroDao dao;
 
+	@Transactional
 	public void salvar() {
+		dao.salvar(livro);
 		System.out.println("Livro Cadastrado: " + livro);
 	}
 
