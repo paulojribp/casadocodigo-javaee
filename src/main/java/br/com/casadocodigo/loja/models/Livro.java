30143e647bca1be12c21543807841fdda4868596
Paulo Jr <paulojribp@gmail.com>
2016-04-18 17:47:32 -0300

    aula3-video5-usando-bean-validation

--- src/main/java/br/com/casadocodigo/loja/models/Livro.java
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -10,6 +10,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 public class Livro {
@@ -17,13 +24,22 @@ public class Livro {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotBlank
 	private String titulo;
+	
 	@Lob
+	@Length(min=10)
+	@NotBlank
 	private String descricao;
+	
+	@DecimalMin("20")
 	private BigDecimal preco;
+	@Min(50)
 	private Integer numeroPaginas;
 	
 	@ManyToMany
+	@Size(min=1)
+	@NotNull
 	private List<Autor> autores = new ArrayList<>();
 	
 	public String getTitulo() {
